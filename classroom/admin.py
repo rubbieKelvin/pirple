1602f16a5d2bc9c7c68c3cc858cae1e6720aad0c
rubbiekelvin <rubbiekelvinvoltsman@gmail.com>
2020-07-04 22:13:07 +0100

    removed __pycache__ & added teacher, student classes

--- classroom/admin.py
+++ classroom/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 
-from .models import User
+from .models import Student
+from .models import Teacher
+from .models import ClassRoom
 
 # Register your models here.
-admin.site.register(User)
+admin.site.register(Student)
+admin.site.register(Teacher)
+admin.site.register(ClassRoom)
--- classroom/models.py
+++ classroom/models.py
@@ -2,10 +2,35 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    
+
     id = models.AutoField(primary_key=True)
 
     firstname = models.CharField(max_length=20)
     lastname  = models.CharField(max_length=20)
     email     = models.EmailField()
     avatar    = models.ImageField(upload_to='uploads/avatar/%Y/%m/%d/', default=models.NOT_PROVIDED)
+
+    def __str__(self):
+        return f"{self.firstname} {self.lastname}"
+
+
+class Student(User):
+    # role = "S"
+    pass
+
+
+class Teacher(User):
+    about = models.TextField(max_length=250)
+
+
+class ClassRoom(models.Model):
+    
+    id = models.AutoField(primary_key=True)
+    
+    name        = models.CharField(max_length=20)
+    description = models.TextField(default="")
+    subject     = models.CharField(max_length=80)
+    teachers    = models.ManyToManyField("User", related_name="classrooms")
+
+    def __str__(self):
+        return f"{self.name}-{self.id}" 
